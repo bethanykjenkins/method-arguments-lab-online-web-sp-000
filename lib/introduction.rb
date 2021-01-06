75c13e06dab745072cfd4374e292aac155be8e99
Bethany Jenkins <bethanykjenkins@rocketmail.com>
2021-01-06 02:41:50 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,5 +3,5 @@ def introduction(name)
 end
 
 def introduction_with_language(name,language)
-  puts "hi, my name is #{name} and I am learning to programe in #{language}"
+  puts "Hi, my name is #{name} and I am learning to programe in #{language}"
 end
